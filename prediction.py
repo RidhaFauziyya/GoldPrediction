c1998ea07974b787ec66fef0dcd125bf2cbe2276
RidhaFauziyya <ridhafauziyyar123@gmail.com>
2024-06-22 11:47:51 +0700

    feat: Add line chart for gold price  and update page styles

--- prediction.py
+++ prediction.py
@@ -2,13 +2,30 @@
 
 import pandas as pd
 import joblib
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, jsonify
 
 app = Flask(__name__)
 #load index.html/ first page. receive input variable from user
 @app.route("/")
 def index():
-	return render_template('index.html')
+    df = pd.read_csv('data.csv')
+    
+    # Convert data to list for Chart.js
+    dates = df['DATE'].tolist()
+    prices_idr = df['IDR'].tolist()
+
+    return render_template('index.html', dates=dates, prices_idr=prices_idr )
+
+@app.route('/data')
+def data():
+    # Load data from Excel file
+    df = pd.read_csv('data.csv')
+    # Prepare data for JSON response
+    data = {
+        'dates': df['Date'].tolist(),
+        'prices_idr' : df['IDR'].tolist()
+    }
+    return jsonify(data)
 
 #load result.html. the result of prediction is presented here. 
 @app.route('/result/', methods=["POST"])
@@ -17,6 +34,4 @@ def prediction_result():
     return render_template('result.html')
 
 if __name__ == "__main__":
-    #host= ip address, port = port number
-    #app.run(host='127.0.0.1', port='5001')
     app.run()
