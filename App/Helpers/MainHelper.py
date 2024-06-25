7d6311e4bfe576c785e1f9df50de163847e2d866
fairuz1 <akbarazariafairuz@gmail.com>
2024-06-26 06:46:08 +0700

    20240626

---
+++ App/Helpers/MainHelper.py
@@ -0,0 +1,99 @@
+from configparser import ConfigParser
+from datetime import datetime, timedelta
+import os, psycopg2, yfinance as yf, pandas as pd, json, requests, math, joblib
+
+base_dir = '\\'.join(os.path.dirname(os.path.abspath(__file__)).split('\\')[:3])
+
+def get_curr_work_dir():
+    directories = dict()
+
+    directories['templates_dir'], directories['static_dir'] = os.path.join(base_dir, 'Views'), os.path.join(base_dir, 'Public')
+    directories['controllers_dir'], directories['helpers_dir'] = os.path.join(base_dir, 'App\\Controllers'), os.path.join(base_dir, 'App\\Helpers')
+    directories['js_dir'], directories['css_dir'] = os.path.join(base_dir, 'Public\\Scripts\\css'), os.path.join(base_dir, 'Public\\Scripts\\js')
+    
+    return directories
+
+def get_gold_data(start_date, end_date, ticker='GC=F'):
+    try:
+        gold_data = yf.download(ticker, start=start_date, end=end_date)
+        gold_data = gold_data.reset_index()
+
+        histories = dict()
+        histories['date_data'] = gold_data['Date'].tolist()
+        histories['close_data'] = gold_data['Close'].tolist()
+
+        '''
+        gold_data = gold_data[['Date','Close']]
+        gold_data.astype({'Date': str}).to_json(os.path.join(base_dir, 'Public\\Dataset\\gold_historical_data.json'), orient='records', lines=False)
+        '''
+
+        return histories
+    except Exception as e:
+        print(e) 
+
+def convert_currencies(amount, curr_currency, prefered):
+    url = f'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{curr_currency}.json'
+    response = requests.get(url)
+    exchange_rate = response.json()
+    converted_amount = "{:0,.2f}".format(float(amount * exchange_rate[curr_currency][prefered]))
+
+    return converted_amount
+
+def get_gold_price_today():
+    url = 'https://api.gold-api.com/price/XAU'
+    response = requests.get(url).json()
+    return "{:0,.2f}".format(float(response['price'])) 
+
+def get_historical_gold_prices(window, date_start=None, date_end=None, ticker='GC=F'):
+    if (date_start == None) and (date_end == None):
+        date_start = datetime.today()-timedelta(days = window * 2)
+        date_end = datetime.today()
+
+    response = yf.download(ticker, start=date_start, end=date_end)
+    if len(response) < window:
+        get_historical_gold_prices(window*2)
+    else:
+        return [list(round(price, 2) for price in response['Close'][-window:])]
+
+def predict_gold_price(model_name, data, prefered_currency='idr'):
+    loaded_model = joblib.load(os.path.join(base_dir, f'App\\Models\\{model_name}'))
+    if prefered_currency != 'usd':
+        result = convert_currencies(loaded_model.predict(data)[0], 'usd', prefered_currency)
+    else:
+        result = "{:0,.2f}".format(float(loaded_model.predict(data)[0]))
+
+    return result
+
+def check_data_window_conf(model_window, algorithm='mlp'):
+    if model_window == 'day':
+        data_window, model_name = 7, f'{algorithm}_model_daily.sav'
+    elif model_window == 'week':
+        data_window, model_name = 11, f'{algorithm}_model_weekly.sav'
+    elif model_window == 'month':
+        data_window, model_name = 10, f'{algorithm}_model_monthly.sav'
+    elif model_window == 'year':
+        data_window, model_name = 10, f'{algorithm}_model_yearly.sav'
+    
+    return data_window, model_name
+
+def load_config(filepath=os.path.join(base_dir, 'database.ini'), section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filepath)
+
+    config = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            config[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filepath))
+
+    return config
+
+def connect(config):
+    try:
+        with psycopg2.connect(**config) as conn:
+            print('Connected to the PostgreSQL server')
+            return conn
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
---
+++ Main.py
@@ -0,0 +1,160 @@
+from App.Helpers import MainHelper as helper
+from flask import Flask, redirect, url_for, request, render_template, jsonify
+from datetime import datetime
+import os, json, pandas as pd
+
+working_dir = helper.get_curr_work_dir()
+app = Flask(__name__, template_folder=working_dir['templates_dir'], static_url_path='', static_folder=working_dir['static_dir'])
+
+@app.route("/")
+def index():
+    return render_template('Templates/index.html')
+
+@app.route("/api/chart/get_gold_data", methods=['GET'])
+def get_gold_data():
+    try:
+        gold_data_path = os.path.join(working_dir['static_dir'], 'Dataset\\gold_historical_data.json') 
+        with open(gold_data_path) as test_file:
+            data = json.load(test_file)
+            dataframe = pd.DataFrame.from_dict(data)
+
+            histories = dict()
+            histories['date_data'] = dataframe['Date'].tolist()
+            histories['close_data'] = dataframe['Close'].tolist()
+        
+        return jsonify(
+            {
+                'response': histories,
+                'status': '200 OK',
+                'messages': 'success'
+            }
+        )
+    except Exception as e:
+        return jsonify(
+            {
+                'response': str(e),
+                'status': '500 ERROR',
+                'messages': 'An Error has occured!'
+            }
+        )
+
+@app.route("/api/chart/get_ranges_gold_data", methods=['GET'])
+def get_ranges_gold_data():
+    data = request.get_json()
+
+    try:
+        return jsonify(
+            {
+                'response': helper.get_gold_data(data['start_date'], data['end_date']),
+                'status': '200 OK',
+                'messages': 'success'
+            }
+        )
+    except Exception as e:
+        return jsonify(
+            {
+                'response': str(e),
+                'status': '500 ERROR',
+                'messages': 'An Error has occured!'
+            }
+        )
+
+@app.route("/api/get_prediction", methods=['GET'])
+def get_prediction():
+    data = request.get_json()
+
+    try:
+        prediction_range = data['model_ranges'].lower().split(' ')[1]
+        pref_currency = data['pred_prefered_currency']
+        model_algorithm = data['model']
+        
+        data_window, model_name = helper.check_data_window_conf(prediction_range, algorithm=model_algorithm)
+        prev_gold_prices = helper.get_historical_gold_prices(data_window)
+
+        return jsonify(
+            {
+                'response': helper.predict_gold_price(model_name, prev_gold_prices, prefered_currency=pref_currency),
+                'status': '200 OK',
+                'messages': 'success'
+            }
+        )
+    except Exception as e:
+        return jsonify(
+            {
+                'response': str(e),
+                'status': '500 ERROR',
+                'messages': 'An error has occured'
+            }
+        )
+    
+@app.route("/api/table/post_prediction_data", methods=['POST'])
+def post_prediction_data():
+    # get user raw json data
+    data = request.get_json()
+
+    try:
+        curr_date = datetime.today().strftime('%Y-%m-%d')
+        prediction_range = data['model_ranges'].lower().split(' ')[1]
+        pref_currency = data['pred_prefered_currency'].lower()
+        model_algorithm = data['model']
+        
+        # get prediction prerequisites
+        data_window, model_name = helper.check_data_window_conf(prediction_range, algorithm=model_algorithm)
+        prev_gold_prices = helper.get_historical_gold_prices(data_window)
+
+        # perform prediction
+        pred_result = helper.predict_gold_price(model_name, prev_gold_prices, prefered_currency=pref_currency)
+
+        # save data
+        with open(os.path.join(working_dir['static_dir'], 'Dataset\\prediction_histories.json')) as file:
+            json_data = json.load(file)
+            dataframe = pd.DataFrame.from_dict(json_data)
+        
+            dataframe.loc[len(dataframe)] = [curr_date, pref_currency, str(helper.get_gold_price_today()), data['model_ranges'], str(pred_result)]
+            dataframe.astype({'Date': str}).to_json(os.path.join(working_dir['static_dir'], 'Dataset\\prediction_histories.json'), orient='records', lines=False)
+
+        # load and return updated json
+        with open(os.path.join(working_dir['static_dir'], 'Dataset\\prediction_histories.json')) as file:
+            updated_json = json.load(file)
+
+        return jsonify(
+            {
+                'response': updated_json,
+                'status': '200 OK',
+                'messages': 'success'
+            }
+        )
+    except Exception as e:
+        return jsonify(
+            {
+                'response': str(e),
+                'status': '500 ERROR',
+                'messages': 'An error has occured'
+            }
+        )
+
+@app.route("/api/table/get_prediction_table_data", methods=['GET'])
+def get_prediction_table_data():
+    # load and return updated json
+    with open(os.path.join(working_dir['static_dir'], 'Dataset\\prediction_histories.json')) as file:
+        updated_json = json.load(file)
+
+    try:
+        return jsonify(
+            {
+                'response': updated_json,
+                'status': '200 OK',
+                'messages': 'success'
+            }
+        )
+    except Exception as e:
+        return jsonify(
+            {
+                'response': str(e),
+                'status': '500 ERROR',
+                'messages': 'An error has occured'
+            }
+        )
+
+if __name__ == "__main__":
+    app.run(debug=True)
